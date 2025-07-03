2e5f0d94a8a64639f85e9818b7f1448d3920e2a4
Dilpreet Singh <dilpreet082023@gmail.com>
2025-07-03 10:43:32 +0530

    initialize payments in nestjs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
